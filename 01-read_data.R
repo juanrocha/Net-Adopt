633c1bd1bcbc202e21ddd0b9d71db87706222d6a
juanrocha <aguilajk@gmail.com>
2022-03-22 11:27:19 +0100

    Quick exploration of Brazil soy data, notes on next steps

---
+++ 01-read_data.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(fs)
+
+
+fls <- fs::dir_ls(path = "~/Documents/Projects/DATA/Trase/BRAZIL_SOY_2.5.1_pc/")
+
+fls |> str_subset(".csv")
+
+dat <- read_csv(file = str_subset(fls, ".csv")) |> 
+    janitor::clean_names()
+
+skimr::skim(dat)
+dat
+
+dat |> 
+    pull(municipality) |> 
+    unique() |> length()
+
+
+dat |> pull(trase_geocode) |> unique()
+
+dat |> 
+    select(year, exporter, importer, country)
+
+dat |> pull(exporter) |> unique() |> length() 
+dat |> pull(importer) |> unique() |> length() 
+dat |> pull(country) |> unique() |> length() 
+
+dat |> 
+    ggplot(aes(land_use_ha)) + 
+    geom_density()+
+    scale_x_log10()
+
