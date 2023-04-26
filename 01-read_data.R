5a7517476bfe0150e2815fd1cd91cd3508406b31
juanrocha <aguilajk@gmail.com>
2023-04-26 14:00:28 +0200

    backing up first explorations

--- 01-read_data.R
+++ 01-read_data.R
@@ -6,28 +6,35 @@ fls <- fs::dir_ls(path = "~/Documents/Projects/DATA/Trase/BRAZIL_SOY_2.5.1_pc/")
 
 fls |> str_subset(".csv")
 
-dat <- read_csv(file = str_subset(fls, ".csv")) |> 
+dat <- read_csv(file = str_subset(fls, ".csv")) |>
     janitor::clean_names()
 
 skimr::skim(dat)
 dat
 
-dat |> 
-    pull(municipality) |> 
+dat |>
+    pull(municipality) |>
     unique() |> length()
 
 
 dat |> pull(trase_geocode) |> unique()
 
-dat |> 
+dat |>
     select(year, exporter, importer, country)
 
-dat |> pull(exporter) |> unique() |> length() 
-dat |> pull(importer) |> unique() |> length() 
-dat |> pull(country) |> unique() |> length() 
+dat |> pull(exporter) |> unique() |> length()
+dat |> pull(importer) |> unique() |> length()
+dat |> pull(country) |> unique() |> length()
 
-dat |> 
-    ggplot(aes(land_use_ha)) + 
+dat |>
+    ggplot(aes(land_use_ha)) +
     geom_density()+
     scale_x_log10()
 
+dat |>
+    filter(year == 2018) |>
+    filter(zero_deforestation_brazil_soy == "COMPANY COMMITMENT") |>
+    group_by(exporter,importer) |>
+    summarize(n = n())
+
+dat |> pull(zero_deforestation_brazil_soy) |> unique()
