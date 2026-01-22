2291e542057233653417988f1799361381d0e96f
juanrocha <aguilajk@gmail.com>
2026-01-22 11:31:51 +0000

    temporal ergms with geometric terms, only last 3yrs

--- 04-stergm.R
+++ 04-stergm.R
@@ -12,17 +12,21 @@ toc() # takes several mins, 252s | 4.2m
 
 
 tic()
-mod2 <- tergm(
+mod4 <- tergm(
     dnet ~ Form(
         ~ edges + 
-            gwb2degree(fixed = TRUE, decay=2.85) +
+            gwb2degree(fixed = TRUE, decay=2.85) + isolates() +
+            #b2nodematch(attr= "cat_commit", diff = TRUE) + #Error
+            b1starmix(k=2, attr="cat_commit", diff=FALSE) + #alternative
             b2star(k = 2, attr = "cat_commit") +
             b2cov("countries") + b2cov("buyers") +
             b1cov("prop_commit") * b2cov("risk") +
             b1cov("soy")  + b2cov("prop_commit") * b1cov("risk") + b2cov("soy")
     ) + Persist(
         ~ edges + 
-            gwb2degree(fixed = TRUE, decay=2.85) +
+            gwb2degree(fixed = TRUE, decay=2.85) + isolates() +
+            #b2nodematch(attr= "cat_commit", diff = TRUE) +
+            b1starmix(k=2, attr="cat_commit", diff = FALSE) +
             b2star(k = 2, attr = "cat_commit") +
             b2cov("countries") + b2cov("buyers") +
             b1cov("prop_commit") * b2cov("risk") +
@@ -35,5 +39,5 @@ toc()
 # 214975.933 sec elapsed | 186Mb object | 2.5 days | mod1
 
 summary(mod2)
-save(mod2, file = "data/termg2.Rda", compress = TRUE)
+save(mod3, file = "data/termg3.Rda", compress = TRUE)
 lobstr::obj_size(mod2)
